b0d8518392c77ab09acc1d637abdd27de9f3f8ec
FrancoMelandri <franco.melandri@gmail.com>
2024-10-01 11:50:02 +0200

    scaffolding project using Dev Containers

---
+++ src/Option.cpp
@@ -0,0 +1,5 @@
+#include "Option.h"
+
+void Option::Hello()
+{
+}
---
+++ src/Option.h
@@ -0,0 +1,10 @@
+#ifndef __LIB_TINY_FP_OPTION__
+#define __LIB_TINY_FP_OPTION__
+
+class Option
+{
+public:
+  void Hello();
+};
+
+#endif
