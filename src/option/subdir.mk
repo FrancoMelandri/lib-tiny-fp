f20c00fc894a28418994f22278f97e4610af754e
FrancoMelandri <franco.melandri@gmail.com>
2024-10-07 18:45:08 +0200

    fixing lib make file

--- src/option/subdir.mk
+++ src/option/subdir.mk
@@ -2,9 +2,3 @@
 
 LIBOBJECTS += \
 	option/Option.o
-
-clean: clean-src
-
-clean-src:
-	-$(RM) ./option/Option.o
-
--- src/try/subdir.mk
+++ src/try/subdir.mk
@@ -2,9 +2,3 @@
 
 LIBOBJECTS += \
 	try/Try.o
-
-clean: clean-src
-
-clean-src:
-	-$(RM) ./try/Try.o
-
